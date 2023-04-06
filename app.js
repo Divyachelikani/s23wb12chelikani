2d354df62141db4cc0987622f97d516c636d3b8f
Divyachelikani <123126195+Divyachelikani@users.noreply.github.com>
2023-04-05 19:15:46 -0500

    done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var biscuitRouter = require('./routes/biscuit');
+
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/biscuit', biscuitRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
